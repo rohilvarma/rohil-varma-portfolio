1b86f26eae44cfb5c114b238d35ce7e8a190d121
rohilvarma11 <rohilvarma96@gmail.com>
2023-12-14 15:22:22 +0530

    Added Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import { Hanken_Grotesk } from 'next/font/google'
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer />
+          <SpeedInsights />
           <Analytics />
         </div>
       </body>
